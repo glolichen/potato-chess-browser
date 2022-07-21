b97969a4003a711e969d4853b9cb421ec154cd59
pidone <jlche18659@gmail.com>
2022-07-21 12:42:24 +0800

    renamed files

--- Pieces.js
+++
@@ -1,9 +0,0 @@
-const PIECES = " PNBRQKpnbrqk";
-function getColor(piece) {
-	return piece > 6;
-}
-function getImage(piece) {
-	let color = getColor(piece) ? "b" : "w";
-	let pieceImage = PIECES[piece].toLowerCase();
-	return color + pieceImage;
-}
