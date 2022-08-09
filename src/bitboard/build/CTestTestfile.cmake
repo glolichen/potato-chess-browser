3b6d2d18c3a588fcc82f42a93258ddb8234f5122
pidone <jlche18659@gmail.com>
2022-08-09 17:56:39 +0800

    use cmake

---
+++ src/bitboard/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jayden/Desktop/Programs/potato-chess/src/bitboard
+# Build directory: /home/jayden/Desktop/Programs/potato-chess/src/bitboard/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
