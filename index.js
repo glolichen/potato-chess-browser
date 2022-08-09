25e9ced00edc2311ee13ff0be93c3323a5a8635f
glolichen <95758447+glolichen@users.noreply.github.com>
2022-08-09 17:46:10 +0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const { app, BrowserWindow } = require("electron");
-
-function createWindow() {
-    let win = new BrowserWindow({});
-    win.maximize();
-    win.loadFile("index.html");
-}
-
-app.on("ready", createWindow);
